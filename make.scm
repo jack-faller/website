39482bb8bdbde2440b4467ab93902377c274d096
Jack Faller <jack.t.faller@gmail.com>
2022-06-15 22:05:02 +0100

    Adds when-outdated macro

--- make.scm
+++ make.scm
@@ -1,10 +1,33 @@
-#!/bin/env guile
+#!/usr/bin/env guile
 !#
 
 (use-modules (ice-9 ftw) (ice-9 regex) (ice-9 textual-ports))
 (define (getfile file) (call-with-input-file file get-string-all))
 (define (putfile file string) (call-with-output-file file (lambda (port) (put-string port string))))
 (define (replace string from to) (regexp-substitute/global #f from string 'pre to 'post))
+
+(define-macro (when-outdated results deps . body)
+  "Run BODY when files any from RESULTS are older than those from DEPS.
+RESULTS is a list of file-names (files need not exist), stat objects (return of
+stat), or numbers from stat:mtime.
+DEPS is of the same type as results, but files must exits.
+BODY can be any number of forms, evaluated and returned given RESULTS are out of
+date. These should then produce new versions of the files referenced by
+RESULTS."
+  (define (mtime-macro file) `(cond ((string? ,file) (stat:mtime (stat ,file)))
+									((number? ,file) ,file)
+									(else (stat:mtime ,file))))
+  (let ((results-gensyms (map (lambda (_) (gensym)) results))
+		(deps-gensyms (map (lambda (_) (gensym)) deps)))
+	`(let (,@(map list results-gensyms results)
+		   ,@(map list deps-gensyms deps))
+	   (when (or ,@(map (lambda (sym)
+						  `(or (not (string? ,sym))
+							   (not (file-exists? ,sym))))
+						results)
+				 (> (max ,@(map mtime-macro deps-gensyms))
+					(min ,@(map mtime-macro results-gensyms))))
+		 (begin ,@body)))))
 
 (define (templatise template-file in-dir out-dir)
   (define (instance input-file output-file)
@@ -22,10 +45,8 @@
 		   (let* ((file-name (substring in-file (string-length in-dir)))
 				  (out-file (string-append out-dir file-name))
 				  (out-mtime (stat:mtime (stat out-file))))
-			 (when (or (not (file-exists? out-file))
-					   (> (max template-mtime (stat:mtime in-statinfo))
-						  out-mtime))
-			   (instance in-file out-file))))
+			 (when-outdated (out-file) (template-mtime in-statinfo)
+							(instance in-file out-file))))
 		 #t)))
 
 (templatise "templates/post.html" "posts/" "data/www/post/")

6af8ec59cbf523098769a3c00be14328054094fe
Jack Faller <jack.t.faller@gmail.com>
2023-09-03 12:50:41 +0100

    Adds thought about privacy

---
+++ thoughts/isnt-privacy-a-bit-weird.scm
@@ -0,0 +1,20 @@
+(post
+ "Isn't Privacy a Bit Weird?"
+ "2023/09/03 12:43 +0100"
+ "I see a lot of people very obsessed with privacy, and I really don't get why."
+ {p
+  A prime example of this would be this guy which basically said: {q Face ID and fingerprint ID are bad because the police can compel you to unlock your phone with those, but not with a password.}.
+  The underlying assumption here is, I think, a common sort of libertarian thought #"&ndash;" that the government is going to create unjust laws so you, as an individual, should have the right to break them and not get caught.
+  That's a bad thing to be thinking.
+  It basically assumes that government is evil so we should move to make it impotent as well, that way it can't hurt us.
+  Thinking that there is absolutely no way to fix the government is the end point of political disenfranchisement.
+  It's anti-democracy in the sense that no democracy can function if people view themselves as fundamentally unable to create good government.}
+ {p
+  Don't get me #"wrong;" I'm not against privacy.
+  I don't think the government should just collect a bunch of data for no reason and I would rather big companies don't know everything about me.
+  But, practically speaking, I've yet to see the benefits of all this mad privacy stuff compared to the effort that goes into it.
+  It's a bit creepy that Mark Zuckerberg knows who my friends are without me creating a Facebook account, but it isn't really doing me any harm.
+  Personally, I don't see the benefit in taking a large number of steps to secure my data in preparation for a police raid on my house.}
+ {p
+  There are a whole bunch of people out there who seem totally obsessed with that.
+  They're willing to spend a lot of money and time, forfeit a lot of personal convenience, just to avoid the possibility of some people knowing relatively innocuous things about them.})

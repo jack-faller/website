4e523d34937d00035bd176793b3809642eb7c67b
Jack Faller <jack.t.faller@gmail.com>
2023-09-08 12:45:43 +0100

    Thought

---
+++ thoughts/new-wonka-has-no-bite.scm
@@ -0,0 +1,9 @@
+(post
+ "New Wonka Has No Bite"
+ "2023/09/08 12:44 +0100"
+ {just
+  I'm sure you've at least been exposed to the saltest in a sudden flurry of one-word-title-that's-the-name-of-the-main-character movies &ndash\; {{a href https://www.youtube.com/watch?v=otNh9bTjXWg} Wonka}.
+  No offence to Timoth&eacute\;e Chalamet, but I don't like it.
+  Gene Wilder's Wonka was a lying child-endangering nutter who who took people on {{a {href https://www.youtube.com/watch?v=XB401RfGMlM}} mad psychedelic boat rides with giant insects while screaming at them}.
+  Depp's was perhaps more deranged (for better or worse).
+  I don't think Chalamet will have any scenes with this kind of bite, and all that's left behind will be a sickly-sweet feel-good movie with little actual substance.})

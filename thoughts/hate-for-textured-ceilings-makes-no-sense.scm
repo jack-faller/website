3439e5b94ee08ba6675716e4d50236461a8f520e
Jack Faller <jack.t.faller@gmail.com>
2023-09-09 22:48:13 +0100

    Adds hate-for-textured-ceilings-makes-no-sense

---
+++ thoughts/hate-for-textured-ceilings-makes-no-sense.scm
@@ -0,0 +1,7 @@
+(post
+ "Hate for Textured Ceilings Makes No Sense"
+ "2023/09/09 22:47 +0100"
+ {just
+  I see a load of people hating on textured ceilings but they are the best ceilings.
+  It's not like you have to look at the ceiling all the time so you barely even notice them.
+  Then, when you do look at the ceiling, textured ones are the most interesting to look at.})

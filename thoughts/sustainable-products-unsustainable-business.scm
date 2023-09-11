e1f7707bfa29d4e61b46d80a24ec05ab55da7f15
Jack Faller <jack.t.faller@gmail.com>
2023-09-11 18:55:50 +0100

    Inits thoughts/sustainable-products-unsustainable-business.scm

---
+++ thoughts/sustainable-products-unsustainable-business.scm
@@ -0,0 +1,7 @@
+(post
+ "Sustainable Products, Unsustainable Business"
+ "2023/09/11 18:36 +0100"
+ {just
+  Rapid growth is expected from new businesses, but this will surely lead to failure if they provide quality products that last a long time.
+  If they expand operations to meet the demand while they grow, they will be left with a huge drop off in sales once everyone that wants their product has bought one.
+  For such businesses, slow growth is much more sustainable than fast growth, yet fast growth is effectively demanded from all new businesses.})

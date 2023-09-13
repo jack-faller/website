f5cb112bdd6bde4d0c658ae7425a098e04e3658a
Jack Faller <jack.t.faller@gmail.com>
2023-09-13 16:48:24 +0100

    Adds i-cant-see-adverts.scm

---
+++ thoughts/i-cant-see-adverts.scm
@@ -0,0 +1,8 @@
+(post
+ "I Can't See Adverts"
+ "2023/09/13 16:48 +0100"
+ {just
+  I've gotten this advert on YouTube with some really annoying music in it a lot recently.
+  It only just occurred to me that I don't know what it's for beyond that it has cars in it.
+  I don't think I know what any adverts are for because I've seen so many that my brain just blocks them out.
+  I would guess most people are like this which makes me wonder how adverts make any money at all.})

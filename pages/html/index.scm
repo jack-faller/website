23b4002e47474e40bb8cf1277d544058cebe91ad
Jack Faller <jack.t.faller@gmail.com>
2023-09-14 16:46:49 +0100

    Re-formats index

--- pages/html/index.scm
+++ pages/html/index.scm
@@ -7,15 +7,21 @@
   I've decided to upload some of those things to the internet, and a major
   location for that is this site. I have too many ideas to keep them all to
   myself, so I hope you can get something from the ones I put here.}
+ (link-heading 2 "all-stuff" "All Stuff")
+ {ul
+  {li Thoughts: {{a {href /thoughts}} see all}, {{a {href /thoughts.rss}} RSS}}
+  {li Blog: {{a {href /posts}} see all}, {{a {href /blog.rss}} RSS}}
+  {li Github: {{a {href https://github.com/jack-faller}} github.com/jack-faller}}
+  {li Email: {{a {href mailto:jack.t.faller@gmail.com}} jack.t.faller@gmail.com}}}
  (link-heading 2 "recent-stuff"
 			   "Recent Stuff"
 			   {join #" (" {{a {href /stuff}} see all}
 					 #", " {{a {href /stuff.rss}} RSS}
 					 #")"})
  {ul #@(map (post->li #t) (at-most 10 public-stuff))}
- (link-heading 2 "all-stuff" "All Stuff")
- {ul
-  {li Thoughts: {{a {href /thoughts}} see all}, {{a {href /thoughts.rss}} RSS}}
-  {li Blog: {{a {href /posts}} see all}, {{a {href /blog.rss}} RSS}}
-  {li Github: {{a {href https://github.com/jack-faller}} github.com/jack-faller}}
-  {li Email: {{a {href mailto:jack.t.faller@gmail.com}} jack.t.faller@gmail.com}}})
+ (link-heading 2 "recent-blog"
+			   "Recent Blog Posts"
+			   {join #" (" {{a {href /posts}} see all}
+					 #", " {{a {href /blog.rss}} RSS}
+					 #")"})
+ {ul #@(map (post->li #f) (at-most 5 public-posts))})

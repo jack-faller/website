b018bf62d0ebe8a3ea3fbc6e961eb267e5dac766
Jack Faller <jack.t.faller@gmail.com>
2023-09-14 16:52:48 +0100

    Adds 4th-wall-of-game-menus.scm

---
+++ thoughts/4th-wall-of-game-menus.scm
@@ -0,0 +1,13 @@
+(post
+ "4th Wall of Game Menus"
+ "2023/09/14 16:43 +0100"
+ {just
+  You might consider menus as a sort of 4{sup th} wall of video games.
+  They exist as interface between the world of the game and the physical reality of consoles.
+  It's quite odd then, that many games present these diegetically &ndash\; as though they actually exist in the world of the game.
+  The {{a {href https://fallout.fandom.com/wiki/Pip-Boy}} Pip Boy} is a good example of this.
+  They player's in-game character will look at a watch-like device on their wrist to access the items they're carrying.
+  It's down-right strange that to get an item out of your backpack you would somehow use a smart watch to select it, but that seems to be the canonical way of doing this in the Fallout games.
+  Another example is MMO menus as seen through Isekai anime.
+  Characters in these shows will often open a stat menu as a strange floating hologram which they take as naturally existing in the world.
+  Games like Undertale can even be observed breaking this 4{sup th} wall when characters from the game interact with the player's menu.})

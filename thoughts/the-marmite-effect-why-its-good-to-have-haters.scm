b5b790fc7fa7d0b93e629db32fccab00af920e47
Jack Faller <jack.t.faller@gmail.com>
2023-09-18 19:46:01 +0100

    Adds the-marmite-effect-why-its-good-to-have-haters.scm

---
+++ thoughts/the-marmite-effect-why-its-good-to-have-haters.scm
@@ -0,0 +1,8 @@
+(post
+ "The Marmite Effect: Why It's Good to Have Haters"
+ "2023/09/18 19:45 +0100"
+ {just
+  If something has a rating of 5/10, you would be inclined to say it's somewhat underwhelming and perhaps not entirely liked.
+  This assumes that the scores are normally distributed.
+  If they are instead distributed according to the inverse &ndash\; a marmite distribution where people love it or hate it and scores cluster around 1 and 10 &ndash\; there will be a much greater absolute number of people who love it.
+  Being controversial actually makes something more liked than something bland, even if the average quality is the same.})

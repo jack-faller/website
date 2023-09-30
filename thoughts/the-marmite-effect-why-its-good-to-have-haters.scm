cd74a0df574423e9d9f0642b934806b339e24c8e
Jack Faller <jack.t.faller@gmail.com>
2023-09-30 23:06:03 +0100

    Updates the-marmite-effect-why-its-good-to-have-haters.scm

--- thoughts/the-marmite-effect-why-its-good-to-have-haters.scm
+++ thoughts/the-marmite-effect-why-its-good-to-have-haters.scm
@@ -5,4 +5,5 @@
   If something has a rating of 5/10, you would be inclined to say it's somewhat underwhelming and perhaps not entirely liked.
   This assumes that the scores are normally distributed.
   If they are instead distributed according to the inverse &ndash\; a marmite distribution where people love it or hate it and scores cluster around 1 and 10 &ndash\; there will be a much greater absolute number of people who love it.
-  Being controversial actually makes something more liked than something bland, even if the average quality is the same.})
+  The average score could be lower for the marmite and it would still have a greater number of devotees.
+  Being controversial actually makes something more liked than being bland.})

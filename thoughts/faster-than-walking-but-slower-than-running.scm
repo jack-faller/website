4dec95ee263fa60e14f6987bf0af1ace605aff6e
Jack Faller <jack.t.faller@gmail.com>
2023-09-30 23:05:53 +0100

    Adds faster-than-walking-but-slower-than-running.scm

---
+++ thoughts/faster-than-walking-but-slower-than-running.scm
@@ -0,0 +1,9 @@
+(post
+ "Faster than Walking but Slower than Running"
+ "2023/09/30 23:04 +0100"
+ {just
+  You know when you have a quest to follow an NPC in a video game and they move just faster than your walk speed but slower than your run speed?
+  I had always just assumed that was necessary.
+  If they moved at just the walk speed it would be too slow and you'd get impatient, but moving at the run speed they might get too far ahead.
+  I had always just dismissed this problem as unsolvable until I thought about it for more than 5 seconds and realised you could just have them go at run speed then slow down if they get too far ahead.
+  So if any video game makes that mistake in the future, they now have no excuse (aside from it looking a bit odd if everyone runs everywhere).})

b4262ecefdd18372ede5d011a8e29e0c072937b4
Jack Faller <jack.t.faller@gmail.com>
2023-10-06 22:41:28 +0100

    Adds the-christian-perspective-on-ai-content.scm

---
+++ thoughts/the-christian-perspective-on-ai-content.scm
@@ -0,0 +1,6 @@
+(post
+ "The Christian Perspective on AI Content"
+ "2023/10/06 22:41 +0100"
+ {just
+  It occurs to me that a common Christian perspective on art is that it's motivated by God and he is the true source of inspiration.
+  In that sense, he could also {q inspire} the random noise which serves as the basis for AI art.})

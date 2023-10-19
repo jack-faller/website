e71506b668eca3fa11ba6ff4cd6d8e5e4bcd5c4f
Jack Faller <jack.t.faller@gmail.com>
2023-10-19 23:50:29 +0100

    Adds thoughts/plastic-recycling-is-useless-technology.scm

---
+++ thoughts/plastic-recycling-is-useless-technology.scm
@@ -0,0 +1,11 @@
+(post
+ "Plastic Recycling Is Useless Technology"
+ "2023/10/19 23:50 +0100"
+ {just
+  There are two possibilities for how society responds to plastic pollution.
+  The fist is that we continue to produce plastic and destroy the world with it.
+  It's impossible to recycle most of that plastic and the recycled stuff usually becomes waste anyway so plastic recycling doesn't help here.
+  The other is that we stop produce plastic.
+  Plastic recycling is useless if there is no plastic.
+  Either way, these stupid plastic recycling project people come up with are useless.
+  They only serve to justify the production of more plastic by pretending it can be recycled and isn't as bad as it is.})

4dd77f57715e668c1a3b0688957aea933791fc86
Jack Faller <jack.t.faller@gmail.com>
2023-11-30 20:44:51 +0000

    Fixes typo

--- thoughts/die-centrist-scum.scm
+++ thoughts/die-centrist-scum.scm
@@ -2,10 +2,9 @@
  "Die Centrist Scum"
  "2023/10/29 01:08 +0100"
  {just
-  I have elected for a rather extremist title as an exercise in irony.
   There are a surprising number of centrists on the Israel/Palestine debate, and I think it exposes their fundamental stupidity.
   Sometimes people are just correct about things, and being against Israel's persecution of Palestinians is one of those instances.
-  The closest situation I can think of is how European settlers treaded the natives of the Americas.
+  The closest situation I can think of is how European settlers treated the natives of the Americas.
   The Europeans were wrong to do all the bad things they did to the natives.
   People in the past have been wrong, so clearly people in the present can be wrong.
   Yet centrists deny this reality.
@@ -13,4 +12,7 @@
   Facts aught to be avoided as they can influence people's opnions.
   Reality is but an inconvenient footnote that might cause uneccessary bias in favour of one side or the other.
   Centrism has replaced logical thought in these people's minds.
-  Blindly applying a strategy of {q take the middle road} usually makes them seem sophisticated and diplomatic, but completely fails when you are talking about something this black and white.})
+  Blindly applying a strategy of {q take the middle road} usually makes them seem sophisticated and diplomatic, but completely fails when you are talking about something this black and white.
+  <br>
+  (Note: I have selected this rather extremist title as an exercise in irony.)})
+

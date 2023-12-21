4fce14f24f548dc3ca250c4a91d7b366ef541750
Jack Faller <jack.t.faller@gmail.com>
2023-12-21 02:07:26 +0000

    Add thoughts/mysteries-of-the-multi-store-gift-card.scm

---
+++ thoughts/mysteries-of-the-multi-store-gift-card.scm
@@ -0,0 +1,15 @@
+(post
+ "Mysteries of the Multi-Store Gift Card"
+ "2023/12/21 02:04 +0000"
+ {just
+  I've seen a few of these gift cards that you can spend in any shop floating around.
+  A gift card is already a pointless thing from the consumer's perspective.
+  The only real reason behind them is to satisfy the requirement that you purchase goods for the annual consumerism events.
+  It still makes sense from the shop's perspective.
+  People who buy giftcards are spending money in the shop that they mightn't otherwise.
+  Some of the money is likely wasted when the recipient of the card doesn't quite meet the face value, or they will be prompted to spend extra to go over.
+  None of these benefits exist for a multi-store gift card.
+  I would assume the money is just held by the card company until its spent at one of the supported shops.
+  It offers nothing for either party involved.
+  It's just money but worse.
+  Why do we have these?})

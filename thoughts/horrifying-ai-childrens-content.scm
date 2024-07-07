8a76cab21326d3c816243261ebecfb3401f8fbbd
Jack Faller <jack.t.faller@gmail.com>
2024-07-07 15:46:49 +0100

    Adds thoughts/horrifying-ai-childrens-content.scm

---
+++ thoughts/horrifying-ai-childrens-content.scm
@@ -0,0 +1,12 @@
+(post
+ "Horrifying AI Children's Content"
+ "2024/07/07 15:43 +0100"
+ {just
+  Having just seen this {{a {href https://youtu.be/1IyALvTlgec}} horrifying AI abomination} advertised to me on YouTube, I'm concerned about the effect of AI on children.
+  I think we can all appreciate that brainrot IPad-baby content is probably bad for children in the same way that social media is probably bad for adults, but this seems uniquely harmful.
+  I think it's because there might, at some point, be genuinely be no human involvement in these videos at all; just AI feeding children whatever makes them click on it.
+  In fact there would be two different AIs at work, training to feed children with the optimally engaging content.
+  Channels will use AI models to mass produce colourful nonsense, then the YouTube AI will create a kind of natural selection, causing these channels to evolve into whatever optimally holds the attention of children.
+  The result is a phenomenon known as brainrot &ndash\; content which uses techniques algorithmically developed to retain viewer's attention and nothing else.
+  It's probably bad to expose children to this stuff, especially at the scale AI now threatens to.
+  There should be some human oversight.})

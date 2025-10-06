f110a39f03505430797cba06ffdfbce7f1fc25a2
Jack Faller <jack.t.faller@gmail.com>
2025-10-06 18:25:36 +0100

    Adds thing I forgot to earlier

---
+++ thoughts/a-theory-on-nigel-farages-fashion-choices.scm
@@ -0,0 +1,10 @@
+(post
+ "A Theory on Nigel Farage's Fashion Choices"
+ "2024/06/24 23:50 +0100"
+ {just
+  For a long time I've assumed that Nigel Farage dresses in hunting gear to go around counsel estates in some out-of-touch attempt to seem common.
+  {q Poor people all work in the fields, so I better wear my country clothes to look like one of them.}
+  This was my rough approximation of his logic.
+  It occurs to me now that there may be a simpler explanation: He wants to look like a rich person.
+  His goal is to appear as an elite who listens to and loves the lower classes, descending from his opulence to guide them to success.
+  His greatest trick is to make people love their rulers and hate their neighbours.})

9235073f6bac40fd1badf5b52c568160a5df5e34
Jack Faller <jack.t.faller@gmail.com>
2025-11-04 23:03:47 +0000

    Fixes weird typo

--- posts/goto-considered-obsolete/named-let.scm
+++ posts/goto-considered-obsolete/named-let.scm
@@ -2,9 +2,9 @@
            (sum 0))
   (if (> i 10)
       sum
-      (loop (+ i 1) ( @ sum i))))
+      (loop (+ i 1) (+ sum i))))
 => (letrec (loop (lambda (i sum)
                    (if (> i 10)
                        sum
-                       (loop (+ i 1) ( @ sum i)))))
+                       (loop (+ i 1) (+ sum i)))))
      (loop 0 0))

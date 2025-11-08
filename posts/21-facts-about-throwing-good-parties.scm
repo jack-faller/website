cd8b40efa43c9d041961f413155021cfa3cd179f
Jack Faller <jack.t.faller@gmail.com>
2025-11-08 20:20:26 +0000

    Adds posts/21-facts-about-throwing-good-parties

---
+++ posts/21-facts-about-throwing-good-parties.scm
@@ -0,0 +1,7 @@
+(use-modules (doclisp) (make))
+(set-reader! doclisp-reader)
+{{repost {version #1}}
+ {title 21 Facts About Throwing Good Parties}
+ {parent https://www.atvbt.com/21-facts-about-throwing-good-parties/}
+ {uuid  42e5668b-3f14-46e2-85bc-92d57d5a40cc}
+ {published #"2025/11/08 20:18 +0000"}}

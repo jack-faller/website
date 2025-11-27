fe1ff8e66ca0b74343e2e2720f0fe36c4ebf9b60
Jack Faller <jack.t.faller@gmail.com>
2025-11-27 10:54:10 +0000

    Adds posts/write-the-slash.scm

---
+++ posts/write-the-slash.scm
@@ -0,0 +1,14 @@
+(use-modules (doclisp) (make))
+(set-reader! doclisp-reader)
+{{thought {version #1}}
+ {title Write the Slash}
+ {uuid 42f3de43-d094-44c8-be48-d06b68f5ae23}
+ {published #"2025/11/27 10:52 +0000"}
+ {description
+  I've heard the advice that one aught not use a slash (/) in formal writing as it is ambiguous whether it means {q and} or {q or}, but I find it works well in cases where either would work.
+  In situations such as a {q carpenter/cabinet maker,} both alternations are describing the same person, so that person is both things—conjunction—but equally both activities are not partaken at the same time—thus disjunction.
+  The slash therefore has use when describing two mutually exclusive properties of the same subject.
+  I think the real source of the complaint is that the slash symbol tends to look rather bad in type setting, for instance in the phrase {q carpenter/cabinet maker}, the slash appears to group {q carpenter/cabinet} where it is actually taking both words after it.
+  I think a reasonable solution to this, is to write a {q carpenter slash cabinet maker.}
+  While some may find this literal use of punctuation disturbing, I note that the slash is often actively pronounced in speech, and is thus more of an abbreviation, similar to ampersand (&), the recommendation for which is use only in cases where space is limited and not in prose.
+  Extending this treatment to slash is only logical.}}

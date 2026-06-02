ce8e13550cfe9d67991d45cea3ed1d195713a5e6
Jack Faller <jack.t.faller@gmail.com>
2026-06-02 23:35:47 +0100

    Adds posts/i-cant-afford-groceries.scm

---
+++ posts/i-cant-afford-groceries.scm
@@ -0,0 +1,22 @@
+(use-modules (doclisp) (make))
+(set-reader! doclisp-reader)
+{{thought {version #1}}
+ {title I Can't Afford Groceries}
+ {published #"2026/06/02 23:35 +0100"}
+ {uuid 4e230e85-aece-4c82-9d19-8ff26b77525a}
+ {description
+  As grocery prices rise, it is an increasingly common political refrain for people to say that they can't afford groceries.
+  Most of them are clearly lying.
+  You can tell by the fact they're alive (and often fat).
+  Someone who can't get food is dead and usually quite skinny.
+  There are some people who suffer genuine food insecurity, these people mostly suffer from some degree of malnutrition rather than abject starvation.
+  Many are even obese.
+  These people are almost never the ones you see complaining about grocery prices.
+  Instead, they are used as a shield by people who are in no danger of malnutrition.
+  They want to complain about how rising food costs prevent them going on holiday or having as many nights out as they'd like, a legitimate political concern, but they also want their issues to be more important than everyone else's.
+  The solution?
+  Say that you can't afford groceries and talk about all the people starving, even though you have basically no clue about the real issues facing this group of people and little care in the actual policies that could help them.
+  (These policies usually involve the person complaining having to pay higher taxes, a prospect to which they object strongly as their entire motivation for complaint was that they wanted to have more disposable income.)
+  I find this attitude deeply offensive.
+  It treats the poor as props, their issues only mentioned when you personally are affected.
+  As soon as inflation goes down, the struggling poor will be forgotten by these people who never really cared.}}

db21fe25892614fc1d4c6d44b7fcf5518959de71
Jack Faller <jack.t.faller@gmail.com>
2026-06-12 15:11:45 +0100

    Adds posts/economic-growth-is-enough-and-only-economic-growth-is-enough.scm

---
+++ posts/economic-growth-is-enough-and-only-economic-growth-is-enough.scm
@@ -0,0 +1,73 @@
+(use-modules (doclisp) (make))
+(set-reader! doclisp-reader)
+{{repost {version #1}}
+ {parent https://lantpritchett.org/wp-content/uploads/2022/05/Basics-legatum-paper_short.pdf}
+ {title Economic Growth Is Enough and Only Economic Growth Is Enough}
+ {uuid 75cfc148-6af1-4218-a005-4d6955193a1f}
+ {published #"2026/06/12 15:04 +0100"}
+ {description *if your objective is to grow things.}
+ {body
+  {p
+   This rather humorous paper claims a significant result in economic and political theory that is undercut by a massive caveat.
+   In rather the fashion of a click-bait headline in a low quality tabloid, the title of this paper seems to indicate the economic growth is some absolute imperative.
+   When we read into the body, we find the substantially more tepid result that economic growth is a necessity if you want to grow the sectors of the economy designated as {q basics.}
+   Yes, you heard it here first, people have more basic stuff if they have more stuff.
+   In fact, it's even worse than that because they show the relationship tails off as GDP grows, so it's questionable whether there would be any significant benefit to basics when countries with higher GDP focus on growth.
+   It could equally be used to argue that non-growth measures become increasingly effective relative to growth as GDP increases.
+   This trivial result is packaged behind a rather misleading headline, I suspect with the goal of being quoted in political discussion by people who haven't read or understood even the abstract.}
+  {p
+   The disproof is, of course, quite trivial.
+   Consider a marginal utility function for basics which becomes zero or negative once needs are sufficiently met.
+   For instance, calories consumed per capita is a measure which quite clearly becomes negative beyond a certain point.
+   The paper's result, that through increasing GDP we can asymptotically increase calories consumed per capita, ignores the question of whether we would actually want to do that, which we would not.
+   This reveals the most pernicious underlying assumption of the paper, which is that growth in basics is always good.
+   We begin assuming growth is good, and end concluding growth is good.}
+  {p
+   This same result holds more generally in all domains of life.
+   Growth is good, but past a certain point you have enough and more is bad.
+   Many people lack this conception of {q enough} and so fall into the trap of growth.
+   We can see this expressed in the paper's mathematics whereby the final resultant measure of basics is assumed to be a weighted average of other measures in which growth must be the objective.
+   When most people have more than enough and a few are in poverty, this paper suggests economic growth might halve the number of people in poverty, but neatly leaves out that the number could be dropped to zero if the people with more than enough accepted less.}
+  {p
+   This is even present in the paper itself as it identifies several metrics, such as emissions and behavioural risk factors (obesity, smoking, drug use), which do not respond to economic growth in the same way as the other factors outlined.
+   This is why they add the caveat which they elect to call {q generality} which requires that the metric be composed of many other metrics so that the other ones they're using can overpower these negative factors, and label them as non-basic to avoid dealing with their uncomfortable implications.}
+  {p
+   Even if you go along with the paper's premise of growth being positive in all basic areas, concluding that we should pursue it as political policy is not correct.
+   On a finite Earth, growth now can lower GDP in the future.
+   The burning of fossil fuels creates substantial short term gains in growth, but in the longer term will cost us dearly.
+   A policy of reducing their use might initially seem to decrease growth, but can be good in the long term.
+   We can even find as much in the paper's conclusion.}
+  {blockquote
+   If the level of GDPPC is {q unsustainable} in the sense that it cannot be sustained and falls in the future on the current (expected) path then the dynamics of GDPPC and basics would get complicated.
+   But nothing we say should be or could be, we hope, interpreted as encouraging countries pursue {q unsustainable} paths of GDPPC.}
+  {p
+   {q Complicated} is quite the understatement.}
+  {p
+   There is an equally strange attempt to justify the unimportance of environmental factors.}
+  {blockquote
+   In our analysis we never decided not to include indicators about the condition of the natural environment and its impact on human wellbeing but rather chose processes for defining basics from which the particular indicators included as basics emerged endogenously.
+   We did not {q ignore} or {q exclude} environmental indicators.}
+  {p
+   I don't think you can, by claiming something to be true (that you didn't ignore environmental factors) , make it so.
+   They claim that the exclusion of these measures makes no difference as they're already accounted for using existing measures.
+   This is a claim which you could very easily substantiate with data and their choice not to is interesting.}
+  {p
+   Finally, they attempt to address arguments about the {q inclusivity} of growth.}
+  {blockquote
+   We just examine the association of basics and GDPPC and do not adjust for inequality or whether the path of economic growth by which the level of GDPPC was achieved was “inclusive growth” or not.
+   [...]
+   That said, in the graphs there is an outlier, Equatorial Guinea (GNQ) which is the obvious exception that proves the rule and shows that one can have (relatively) high levels of GDPPC driven by mineral (oil) exports that is sufficiently “exclusive” that it does not lead to gains in basics.}
+  {p
+   Call me a stickler, but the phrase {q the exception that proves the rule} should probably not appear in a scientific paper.}
+  {p
+   An admission can even be found in the paper that {q we are not making some silly claim like {q it is worth doing anything to promote growth}.}
+   Well then maybe you shouldn't have picked the title you did, given that any policy maker or pundit who reads the title of this paper may use it as evidence to do precisely that.}
+  {p
+   In short, we have a paper that fails on many important metrics.
+   But by the right metrics, it is quite successful.
+   Correctly stated, I think the conclusions drawn here are both accurate and useful.
+   With respect to the poorer countries of which this paper talks, it makes a compelling and reasonable argument that developing their economies would be the most effective method to improve standards of living.
+   This is in contrast to other charitable efforts like distributing food and clothing, which may not develop the economy and can be counterproductive in some cases.
+   Of course doing this most effectively would put a damper on the economic growth of richer countries, a point which is very important yet not addressed.
+   The reverse is also true.
+   Pursuing a policy of growth in rich countries has the substantial possibility of decreasing growth in poor countries.}}}

ae1a26cbcedf4de41eb84cc322aca17401bea701
Jack Faller <jack.t.faller@gmail.com>
2026-07-02 01:33:40 +0100

    Adds ZLib and missing configure flags to GoAccess

--- guix.scm
+++ guix.scm
@@ -19,10 +19,13 @@
       (sha256
        (base32 "1nyknlp6pg8wfb1lixnjgmfg6b0ra0fx36s06z7jhc1397g9jkwz"))))
    (build-system gnu-build-system)
+   (arguments
+    (list
+     #:configure-flags #~'("--with-zlib" "--with-openssl" "--with-getline" "--enable-geoip=mmdb" "--enable-utf8")))
    (native-inputs
     (map specification->package '("autoconf" "automake" "gettext")))
    (inputs
-    (map specification->package '("ncurses" "libmaxminddb" "openssl")))
+    (map specification->package '("ncurses" "libmaxminddb" "openssl" "zlib")))
    (home-page "https://goaccess.io/")
    (synopsis "GoAccess is an open source real-time log analyzer and interactive viewer that runs in a terminal in *nix systems or through your browser.")
    (description "GoAccess is an open source, real-time web log analyzer and interactive viewer that runs in a terminal on *nix systems or directly in your browser. Designed with system administrators, DevOps engineers, and security professionals in mind, it delivers fast, actionable HTTP statistics and visual server reports on the fly. GoAccess parses your web server logs in real time and presents the data directly in the terminal or via a live HTML dashboard, making it easy to monitor traffic, detect anomalies, and troubleshoot issues instantly.")

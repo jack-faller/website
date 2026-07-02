c58490b6ac1a8def8ac8833dbf5f85bd12a8fa7a
Jack Faller <jack.t.faller@gmail.com>
2026-07-02 02:05:22 +0100

    Adds posts/50-years-of-travel-tips.scm

---
+++ posts/50-years-of-travel-tips.scm
@@ -0,0 +1,7 @@
+(use-modules (doclisp) (make))
+(set-reader! doclisp-reader)
+{{repost {version #1}}
+ {title 50 Years of Travel Tips}
+ {published #"2026/07/02 02:04 +0100"}
+ {uuid 4a17622c-d371-47ae-bc1d-27bc8dfee5e6}
+ {parent https://kk.org/thetechnium/50-years-of-travel-tips/}}

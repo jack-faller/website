250ba602ac76b95101b9be9d09656ba9347e2e6c
Jack Faller <jack.t.faller@gmail.com>
2026-08-15 02:26:11 +0100

    Adds posts/a-comparison-of-methods-for-soy-bean-hulling.scm

---
+++ posts/a-comparison-of-methods-for-soy-bean-hulling.scm
@@ -0,0 +1,43 @@
+(use-modules (doclisp) (make))
+(set-reader! doclisp-reader)
+{{post {version #1}}
+ {title A Comparison of Methods for Soy Bean Hulling}
+ {uuid e0502fce-f0bb-4a5b-a810-7aa844ed752d}
+ {description
+  I have been making my own soy milk for some time.
+  Hulling the beans improves the colour and flavour of the milk, but there are many methods.
+  Here I present my experiences with those I've tried.}
+ {body
+  #(link-heading 2 "by-hand" "By Hand")
+  {p
+   Rub the soaked beans between your fingers and palms to remove skins.}
+  {p
+   This method is taxing.
+   It can be somewhat painful if the beans are still a little hard.
+   It is slow and does not remove the skins well, taking much longer as there are fewer unskinned beans left in the pot that would need to be picked out.}
+  #(link-heading 2 "food-processor-with-dough-paddle" "Food Processor with Dough Paddle")
+  {p
+   Run the soaked beans through a food processor with the dough paddle attachment.
+   The blunt paddle splits the beans and separates the skins.
+   The hulls are then skimmed off by putting the beans in a bowl and repeatedly filling it with water, then emptying off those hulls which rise to the surface.}
+  {p
+   This method does not remove all the hulls and can take some time as it may take multiple batches.
+   The beans will not be split well if too many are put in the processor and can be annoying to remove.
+   Some hulls are left.}
+  #(link-heading 2 "malt-mill" "Malt Mill")
+  {p
+   I used a {{a {href https://www.the-home-brew-shop.co.uk/products/brewferm-grain-grizzly-cast-iron-malt-mill}} Brewferm Grain Grizzly Cast Iron Malt Mill} to split the dry beans before soaking, then skimmed after.}
+  {p
+   This method requires a large piece of dedicated hardware which costs about £40.
+   The mill itself was cheap and so had a strange coating which might flake off into food, and being cast iron it can rust.
+   As for splitting the beans, it did this very quickly.
+   It did not clamp well to the worktop because the clamp was shallow and would move around somewhat so the body had to be held down with one hand while the other turned the crank.
+   All but a couple of the beans are cracked but the limit on this method is how many of the hulls you can skim off.}
+  #(link-heading 2 "wondermill-junior" "WonderMill Junior")
+  {p
+   Method is the same as the malt mill.}
+  {p
+   This mill costs about £300, but it has a wider variety of non-soy uses than the malt mill, which is how I justify purchasing it as I would like to use it for making nut butters also.
+   The auger in this mill seems to feed the beans in slower than the cheaper model, which means it takes about twice as long to split them (a few minutes for 300g) but the crank is easier to turn as fewer beans enter the mechanism at once.
+   The build quality is much higher than the cheaper mill and the clamping mechanism is better which makes it easier to attach to different kinds of counter tops and it wouldn't move around as much.
+   Results are much the same as previous method.}}}
